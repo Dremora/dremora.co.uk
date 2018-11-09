092632651a80a4ca099ae91659c01fcd0a8b9cc2
Kirill Korolyov <kirill.korolyov@gmail.com>
2018-11-09 12:48:49 +0000

    Change background color

--- styles/global.js
+++ styles/global.js
@@ -1,4 +1,5 @@
 import { css } from 'styled-components';
+import { darken, lighten } from 'polished';
 
 export default css`
   html {
@@ -8,7 +9,8 @@ export default css`
   body {
     font-family: Lato, sans-serif;
     line-height: 1.3;
-    background: #e46000;
+    ${'' /* background: #e46000; */}
+    background: radial-gradient(#e46000, ${darken(0.2, '#e46000')});
     margin: 0;
     color: #ffe6e6;
     font-weight: 900;
